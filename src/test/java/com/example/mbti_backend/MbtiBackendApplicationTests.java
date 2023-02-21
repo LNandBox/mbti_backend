aa0ad14d7af96be0ebb1ce998918fbbae90ce61b
이승민 <sml7179@naver.com>
2023-02-21 10:27:58 +0900

    project init

---
+++ src/test/java/com/example/mbti_backend/MbtiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbti_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
